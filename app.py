5a1012412d401765b21b8af111e64da166bc8660
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2023-12-18 20:36:17 -0300

    a

--- app.py
+++ app.py
@@ -143,9 +143,8 @@ fig_product_sales.update_layout(plot_bgcolor="rgba(0,0,0,0)",xaxis=(dict(showgri
 #--------------------------------------------------------------------------------------------------
 # Vendas por dia no mês
 vendas_diames = df_selection.groupby(by=["Dia Mês"])['Total'].sum().reset_index()
-vendasmes = px.area(vendas_diames,x="Dia Mês",y="Total",title="Vendas No Mês")
+vendasmes = px.area(vendas_diames,x="Dia Mês",y="Total",title="Vendas No Mês",color="#0083B8")
 
-vendasmes.update_layout(xaxis=dict(tickmode="linear"),plot_bgcolor="rgba(0,0,0,0)",yaxis=(dict(showgrid=False)),)
 
 #--------------------------------------------------------------------------------------------------
 #Gráficos
