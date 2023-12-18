c87f073d5d60896ae6ea2e8d2ecc86759c4576f7
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2023-12-18 20:15:17 -0300

    a

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ fig_product_sales.update_layout(plot_bgcolor="rgba(0,0,0,0)",xaxis=(dict(showgri
 
 #--------------------------------------------------------------------------------------------------
 # Vendas por hora
-vendas_diames = df_selection.groupby(by=["Dia Mês"]).count()
+vendas_diames = df_selection.groupby(by=["Dia Mês"])
 fig_hourly_sales = px.area(
     vendas_diames,
     x=vendas_diames.index,
