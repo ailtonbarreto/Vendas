3185d6ff252998446addf78c964f3b04435bc857
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-05-22 21:22:02 -0300

    upgrade

--- app.py
+++ app.py
@@ -169,6 +169,7 @@ vendas_lojas.update_yaxes(showgrid=False)
 with col7:
     st.plotly_chart(vendas_lojas,use_container_width=True)
 with col8:
+    st.subheader("Ranking de Produtos",anchor=False)
     st.dataframe(vendas_produto,use_container_width=True)
 with col9:
     st.plotly_chart(vendasmes, use_container_width=True)
