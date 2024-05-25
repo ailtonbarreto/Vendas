976f27352652ca01a1faf00b77b808c4237a334b
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-05-25 16:13:44 -0300

    updated

--- app.py
+++ app.py
@@ -195,18 +195,6 @@ borderselect = """
 st.markdown(borderselect,unsafe_allow_html=True)
 
 
-cabecalho = """
-    <style>
-    [class="st-emotion-cache-ocqkz7 e1f1d6gn5"]
-    {
-    posistion: relative;
-    height: 200px;
-    }
-    </style>
-"""
-st.markdown(cabecalho,unsafe_allow_html=True)
-
-
 detalhes = """
     <style>
     [class="modebar-container"]
