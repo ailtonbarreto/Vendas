c7f228a39159bef376e787282ea055f47bb1b7a6
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-08-06 10:55:38 -0300

    a

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def get_data_from_excel():
     df["Dia Mês"] = pd.to_datetime(df["Data"]).dt.day
     return df
 
-df = get_data_from_excel()
+# df = get_data_from_excel()
 #--------------------------------------------------------------------------------------------------
 #Definir mês
 
