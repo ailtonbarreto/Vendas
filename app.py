8a3146b6ac972fe9f33f554af53a7293d96c31c0
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-09-15 00:03:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import streamlit as st
 
 st.set_page_config(page_title="Vendas Dashboard", page_icon=":bar_chart:", layout="wide")
 
-with open("style.css") as f:
-    st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html = True)
+# with open("style.css") as f:
+#     st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html = True)
 
 
 #--------------------------------------------------------------------------------------------------
