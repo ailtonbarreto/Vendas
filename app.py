4d620b36fc153527d920a534760d4db020ae6630
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-10-19 03:07:08 -0300

    updated

--- app.py
+++ app.py
@@ -146,6 +146,7 @@ grafico_semana.update_traces(textfont=dict(size=15,color='#ffffff'),textposition
 
 ranking_vendedor = df_selection.groupby(by=["Vendedor"])[["Total"]].sum().sort_values(by="Total",ascending=True)
 ranking_vendedor["Ranking"] = ranking_vendedor["Total"].rank(ascending=False,method="min")
+ranking_vendedor = ranking_vendedor[["Ranking","Vendedor","Total"]]
 ranking_vendedor = ranking_vendedor.sort_values("Total",ascending=False)
 ranking_vendedor["Total"] = ranking_vendedor["Total"].apply(lambda x: f'R$ {x:,.2f}')
 
